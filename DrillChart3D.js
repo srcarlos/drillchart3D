52a0663fd1854b79e875297d6ecdfe21d6f28e1b
Carlos Marcano <clmg1010@gmail.com>
2016-06-26 01:12:21 -0400

    basic functions to using THREEJS

--- DrillChart3D.js
+++ DrillChart3D.js
@@ -60,3 +60,104 @@ DrillChart3D.mesh   = {};
 DrillChart3D.cube   = {};
 
 
+
+
+DrillChart3D.init = function() {
+
+    console.log('DrillChart3D VERSION 001-2016');
+
+    // SCENE
+    DrillChart3D.scene = new THREE.Scene();
+    // CAMERA
+    DrillChart3D.SCREEN_WIDTH = window.innerWidth;
+    DrillChart3D.SCREEN_HEIGHT = window.innerHeight;
+
+
+    DrillChart3D.scene.fog = new THREE.FogExp2(0xcce0ff, 0.0003);
+
+
+
+    DrillChart3D.VIEW_ANGLE = 45,
+    DrillChart3D.ASPECT = DrillChart3D.SCREEN_WIDTH / DrillChart3D.SCREEN_HEIGHT,
+    DrillChart3D.NEAR = 1,
+    DrillChart3D.FAR = 20000;
+
+    // POSITION
+    DrillChart3D.camera = new THREE.PerspectiveCamera(DrillChart3D.VIEW_ANGLE, DrillChart3D.ASPECT, DrillChart3D.NEAR, DrillChart3D.FAR);
+
+
+    DrillChart3D.scene.add(DrillChart3D.camera);
+    DrillChart3D.camera.position.set(DrillChart3D.viewPositions.x*0.9, DrillChart3D.viewPositions.y*1.1, DrillChart3D.viewPositions.z*1.5);
+    DrillChart3D.camera.position.set(3932, 4500, 5470);
+    DrillChart3D.camera.lookAt(DrillChart3D.scene.position);
+
+
+    // RENDERER
+    if (Detector.webgl)
+        DrillChart3D.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true
+        });
+    else
+        DrillChart3D.renderer = new THREE.CanvasRenderer();
+    DrillChart3D.renderer.setSize(DrillChart3D.SCREEN_WIDTH, DrillChart3D.SCREEN_HEIGHT);
+
+    //CONTAINER
+    DrillChart3D.container = document.getElementById('container');
+    DrillChart3D.container.appendChild(DrillChart3D.renderer.domElement);
+
+
+    // EVENTS
+    THREEx.WindowResize(DrillChart3D.renderer, DrillChart3D.camera);
+    THREEx.FullScreen.bindKey({
+        charCode: 'm'.charCodeAt(0)
+    });
+
+
+    // SKYBOX
+    var skyBoxGeometry = new THREE.CubeGeometry(10000, 10000, 10000);
+    var skyBoxMaterial = new THREE.MeshBasicMaterial({
+        color: 0x0f0f0f0,
+        side: THREE.BackSide
+    });
+    var skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
+    DrillChart3D.scene.add(skyBox);
+
+
+    // CONTROLS
+    DrillChart3D.controls = new THREE.OrbitControls(DrillChart3D.camera, DrillChart3D.renderer.domElement);
+
+
+
+
+};
+
+
+
+
+
+// Animate the elements
+// For rendering
+DrillChart3D.animate = function() {
+    requestAnimationFrame(DrillChart3D.animate);
+    DrillChart3D.render();
+    DrillChart3D.update();
+
+};
+// Rendering function
+DrillChart3D.render = function() {
+
+    // For rendering
+    DrillChart3D.renderer.autoClear = false;
+    DrillChart3D.renderer.clear();
+    DrillChart3D.renderer.render(DrillChart3D.scene, DrillChart3D.camera);
+};
+
+DrillChart3D.update = function() {
+
+
+};
+
+
+DrillChart3D.init();
+DrillChart3D.animate();
